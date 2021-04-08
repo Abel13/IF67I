06dec8aa88ada246590de67b1b49f9b3882cdf3b
Abel Dutra <abel.o.d@outlook.com>
2021-04-07 23:46:41 -0300

    feat: added file upload

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -1,8 +1,13 @@
 class FileController {
   async store(req, res) {
-    const { filename: file } = req.file;
-
-    return res.json(req.file);
+    const { filename } = req.file;
+    if (filename) {
+      console.log("ok");
+      return res.json({ url: "http://localhost:3333/files/" + filename });
+    } else {
+      console.log("error");
+      res.error(400).json({ message: "Falha ao salvar a imagem!" });
+    }
   }
 }
 
