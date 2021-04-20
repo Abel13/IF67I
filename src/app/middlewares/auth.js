c2c3e227d8425812803da1b357743422da58e6ed
Abel Dutra <abel.o.d@outlook.com>
2021-04-20 20:54:30 -0300

    feat: changed to middleware jwt-express

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,25 +1,34 @@
-import jwt from "jsonwebtoken";
+import jwt from "jwt-express";
 
 import User from "../models/User";
 
 import authConfig from "../../config/auth";
-import { promisify } from "util";
 
-export default async (req, res, next) => {
+const checkAuth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
     return res.status(401).json([{ message: "Token não enviado!" }]);
   }
 
-  const [, token] = authHeader.split(" ");
-
   try {
-    const { secure_id } = await promisify(jwt.verify)(token, authConfig.secret);
-    const user = await User.findOne({ where: { secure_id } });
+    const user = await User.findOne({
+      where: { secure_id: req.jwt.payload.secure_id },
+    });
+
     req.user = user;
-    return next();
+    next();
   } catch (error) {
     return res.status(401).json([{ message: "Token inválido!" }]);
   }
 };
+
+const middleware = [
+  jwt.init(authConfig.secret, {
+    cookies: false,
+  }),
+
+  checkAuth,
+];
+
+export default middleware;
