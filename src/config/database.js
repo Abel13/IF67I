1ceff52e4be331959109757725be372b4bdbd75a
Abel Dutra <abel.o.d@outlook.com>
2021-04-21 00:05:53 -0300

    fix: add ssl

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,7 @@ module.exports = {
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   dialectOptions: {
-    // ssl: true,
+    ssl: true,
   },
   define: {
     timestamps: true,
